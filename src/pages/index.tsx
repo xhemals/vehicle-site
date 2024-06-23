93145a62c94b435c6856fbcb0704a6a2520925fe
Jamie Speller <j@miee.uk>
2024-06-23 07:15:54 +0100

    Remove ModeToggle component from Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import { ModeToggle } from "@/components/ui/mode-toggle";
 import NumberPlateSearch from "@/components/number-plate/numer-plate-search";
 
 export default function Home() {
 	return (
 		<>
 			<NumberPlateSearch />
-			<ModeToggle />
 		</>
 	);
 }
