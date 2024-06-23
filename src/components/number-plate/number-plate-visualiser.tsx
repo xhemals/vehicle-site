14b503845c2b8b1913a996f2ede1d28f590f8e2a
Jamie Speller <j@miee.uk>
2024-06-23 19:39:53 +0100

    Add more info to vehicle lookup

--- src/components/number-plate/number-plate-visualiser.tsx
+++ src/components/number-plate/number-plate-visualiser.tsx
@@ -20,7 +20,7 @@ export default function NumberPlate({ reg }: { reg: string }) {
 	}, [reg]);
 
 	return (
-		<div className="flex h-[55.5px] w-[260px] flex-col items-center justify-center rounded-lg bg-plate md:h-[111px] md:w-[520px]">
+		<div className="flex h-[55.5px] w-[260px] flex-col items-center justify-center rounded-lg bg-plate md:h-[111px] md:w-[520px] select-none">
 			<div className="flex gap-[16.5px] md:gap-[33px] font-plate text-[39.5px] text-plate-text md:text-[79px]">
 				<span>{formattedReg[0]}</span>
 				<span>{formattedReg[1]}</span>
--- src/pages/vrm/[reg].tsx
+++ src/pages/vrm/[reg].tsx
@@ -11,12 +11,32 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
 
-type dvlaData = {
+import { data } from "autoprefixer";
+
+type dvlaDataType = {
 	taxStatus: string;
 	motStatus: string;
 	taxDueDate: string;
 	motExpiryDate: string;
+	yearOfManufacture: string;
+	colour: string;
+	dateOfLastV5CIssued: string;
+};
+
+type motDataType = {
+	motTestDueDate: string;
+	make: string;
+	model: string;
 };
 
 export async function getServerSideProps({
@@ -32,7 +52,6 @@ export async function getServerSideProps({
 		reg: string;
 	};
 	const validReg = isValid.validReg;
-	console.log(validReg);
 	let carData;
 	if (validReg) {
 		carData = (await DbCarInfo(upperCaseReg)) as {
@@ -63,18 +82,24 @@ export default function VehicleInfo({
 }) {
 	const router = useRouter();
 	const dataNeeded = useMemo(() => {
-		const motData = carData.motData ?? {};
-		const dvlaData = (carData.dvlaData as dvlaData) ?? {};
+		const motData = (carData.motData as motDataType) ?? {};
+		const dvlaData = (carData.dvlaData as dvlaDataType) ?? {};
 		return {
 			taxStatus: dvlaData.taxStatus ?? "Unknown",
 			taxDueDate: formatDate(dvlaData.taxDueDate ?? "Unknown"),
-			motStatus: dvlaData.motStatus ?? "Unknown",
-			motExpiryDate: formatDate(dvlaData.motExpiryDate ?? "Unknown"),
+			motStatus: motData.motTestDueDate
+				? "Valid"
+				: dvlaData.motStatus ?? "Unknown",
+			motExpiryDate: formatDate(
+				dvlaData.motExpiryDate ?? motData.motTestDueDate ?? "Unknown",
+			),
+			make: motData.make ?? "Unknown",
+			model: motData.model ?? "Unknown",
+			year: dvlaData.yearOfManufacture ?? "Unknown",
+			colour: dvlaData.colour ?? "Unknown",
+			v5cIssued: formatDate(dvlaData.dateOfLastV5CIssued ?? "Unknown"),
 		};
 	}, [carData]);
-	// console.log(motData);
-	// console.log(dvlaData);
-	// console.log(validReg);
 
 	useEffect(() => {
 		if (!validReg) {
@@ -105,7 +130,9 @@ export default function VehicleInfo({
 						</CardHeader>
 						<CardContent className="md:pb-3 p-3 pt-0 md:text-2xl text-base flex flex-col items-center">
 							<div className="flex flex-col items-center w-fit">
-								<span>{dataNeeded.taxStatus}</span>
+								<span className="font-bold">
+									{dataNeeded.taxStatus}
+								</span>
 								<span className="text-xs md:pt-1">
 									{checkIfExpired(dataNeeded.taxDueDate)
 										? "Expired"
@@ -125,7 +152,9 @@ export default function VehicleInfo({
 						</CardHeader>
 						<CardContent className="md:pb-3 p-3 pt-0 md:text-2xl text-base flex flex-col items-center">
 							<div className="flex flex-col items-center w-fit">
-								<span>{dataNeeded.motStatus}</span>
+								<span className="font-bold">
+									{dataNeeded.motStatus}
+								</span>
 								<span className="text-xs md:pt-1">
 									{checkIfExpired(dataNeeded.motExpiryDate)
 										? "Expired"
@@ -138,13 +167,77 @@ export default function VehicleInfo({
 				</CardContent>
 			</Card>
 			<div>
-				<Card>
-					<CardHeader className="md:px-6 p-3 pb-0 pt-2">
-						<CardTitle className="md:text-xl text-sm">
+				<Card className="md:w-[450px] w-[260px]">
+					<CardHeader className="pb-3 pt-4">
+						<CardTitle className="md:text-xl text-base">
 							Vehicle Information
 						</CardTitle>
 					</CardHeader>
-					<CardContent className="flex flex-col gap-2 items-center"></CardContent>
+					<CardContent className="flex flex-col gap-3 items-center md:pb-3 p-3 pt-0 ">
+						<Table>
+							<TableBody className="text-left md:text-base text-sm">
+								<TableRow>
+									<TableCell>
+										<div className="flex justify-between w-full items-center">
+											<span>Make:</span>
+											<span className="text-right font-extrabold">
+												{dataNeeded.make}
+											</span>
+										</div>
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										<div className="flex justify-between w-full items-center">
+											<span>Model:</span>
+											<span className="text-right font-extrabold">
+												{dataNeeded.model}
+											</span>
+										</div>
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										<div className="flex justify-between w-full items-center">
+											<span>Year:</span>
+											<span className="text-right font-extrabold">
+												{dataNeeded.year}
+											</span>
+										</div>
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										<div className="flex justify-between w-full items-center">
+											<span>Colour:</span>
+											<span className="text-right font-extrabold flex justify-end items-center gap-2">
+												<Card
+													style={{
+														backgroundColor:
+															dataNeeded.colour,
+													}}
+													className="w-6 h-6"
+												>
+													<CardContent></CardContent>
+												</Card>
+												{dataNeeded.colour}
+											</span>
+										</div>
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										<div className="flex justify-between w-full items-center">
+											<span>V5C Issued:</span>
+											<span className="text-right font-extrabold">
+												{dataNeeded.v5cIssued}
+											</span>
+										</div>
+									</TableCell>
+								</TableRow>
+							</TableBody>
+						</Table>
+					</CardContent>
 				</Card>
 			</div>
 		</div>
@@ -180,15 +273,11 @@ const checkIfExpired = (dateString: string) => {
 	const currentDate = new Date();
 	dueDate.setHours(0, 0, 0, 0);
 	currentDate.setHours(0, 0, 0, 0);
-	console.log(dueDate);
-	console.log(currentDate);
 
 	// Check if the date is expired
 	if (dueDate < currentDate) {
-		console.log("Expired");
 		return true;
 	} else {
-		console.log("Not expired");
 		return false;
 	}
 };
