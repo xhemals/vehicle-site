24937c4eb145b1b7bfcb75968fad24c299b72d0d
Jamie Speller <j@miee.uk>
2024-06-27 00:31:30 +0100

    Add error handling if the car has no recorded mileage

--- src/components/ui/vehicle-info/ui-mileage.tsx
+++ src/components/ui/vehicle-info/ui-mileage.tsx
@@ -1,5 +1,5 @@
-import { Card, CardHeader, CardTitle, CardContent } from "../card";
-import { Table, TableBody, TableRow, TableCell } from "../table";
+import { Card, CardContent } from "@/components/ui/card";
+import { Table, TableBody, TableRow, TableCell } from "@/components/ui/table";
 import DistanceToMoon from "./ui-distance-to-moon";
 
 type passedMotMilesType = {
@@ -34,97 +34,105 @@ export default function UIMileage({
 		Number(vehicleData.mileage.allPassedMotMiles[0]?.odometerValue) ?? "Unknown";
 
 	return (
-		<Card className="md:min-w-[450px] md:max-w-[450px] w-full h-fit md:min-h-[466px]">
-			<CardHeader className="pb-3 pt-4">
-				<CardTitle className="md:text-xl text-base">Mileage</CardTitle>
-			</CardHeader>
-			<CardContent className="flex flex-col gap-3 items-center md:pb-3 p-3 pt-0">
-				<Table>
-					<TableBody className="text-left md:text-base text-sm">
-						<TableRow>
-							<TableCell>
-								<div className="flex justify-between w-full">
-									<span className="text-nowrap h-full">Last known mileage:</span>
-									<span className="flex flex-col gap-1 text-right font-extrabold">
-										{vehicleData.motInfo.hasHadMot &&
-										vehicleData.mileage.allPassedMotMiles?.[0] ? (
-											<>
-												<span>
-													{currentMileage} {vehicleData.mileage.motOdometerUnit} on
-												</span>{" "}
-												<span>{vehicleData.motInfo.lastMotTestDateLong}</span>
-											</>
-										) : (
-											"Unknown"
-										)}
-									</span>
-								</div>
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell>
-								<div className="flex justify-between w-full">
-									<span className="text-nowrap">Mileage Last Year:</span>
-									<span className="text-right font-extrabold">
-										{vehicleData.motInfo.hasHadMot &&
-										vehicleData.mileage.allPassedMotMiles?.[0]
-											? `${Number(vehicleData.mileage.allPassedMotMiles[0].odometerValue) - Number(vehicleData.mileage.allPassedMotMiles[1]?.odometerValue)}
+		<>
+			{vehicleData.motInfo.hasHadMot ? (
+				<Card className="md:min-w-[450px] md:max-w-[450px] w-full h-fit">
+					<CardContent className="flex flex-col gap-3 items-center md:pb-3 p-3 pt-3">
+						<Table>
+							<TableBody className="text-left md:text-base text-sm">
+								<TableRow>
+									<TableCell>
+										<div className="flex justify-between w-full">
+											<span className="text-nowrap h-full">Last known mileage:</span>
+											<span className="flex flex-col gap-1 text-right font-extrabold">
+												{vehicleData.mileage.allPassedMotMiles?.[0] ? (
+													<>
+														<span>
+															{currentMileage} {vehicleData.mileage.motOdometerUnit} on
+														</span>{" "}
+														<span>{vehicleData.motInfo.lastMotTestDateLong}</span>
+													</>
+												) : (
+													"Unknown"
+												)}
+											</span>
+										</div>
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										<div className="flex justify-between w-full">
+											<span className="text-nowrap">Mileage Last Year:</span>
+											<span className="text-right font-extrabold">
+												{vehicleData.motInfo.hasHadMot &&
+												vehicleData.mileage.allPassedMotMiles?.[0]
+													? `${Number(vehicleData.mileage.allPassedMotMiles[0].odometerValue) - Number(vehicleData.mileage.allPassedMotMiles[1]?.odometerValue)}
                                         ${vehicleData.mileage.motOdometerUnit}`
-											: "Unknown"}
-									</span>
-								</div>
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell>
-								<div className="flex justify-between w-full">
-									<span className="text-nowrap">Average Yearly Mileage:</span>
-									<span className="text-right font-extrabold">
-										{vehicleData.motInfo.hasHadMot
-											? `${averageYearlyMileage} ${vehicleData.mileage.motOdometerUnit}/year`
-											: "Unknown"}
-									</span>
-								</div>
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell>
-								<div className="flex justify-between w-full">
-									<span className="text-nowrap">Estimated Current Mileage:</span>
-									<span className="text-right font-extrabold">
-										{vehicleData.motInfo.hasHadMot
-											? `${estimatedMileage(weeksSinceLastMot(vehicleData.motInfo.lastMotTestDateShort), averageYearlyMileage, currentMileage)} ${vehicleData.mileage.motOdometerUnit}`
-											: "Unknown"}
-									</span>
-								</div>
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell>
-								<div className="flex flex-col gap-1.5 justify-between w-full items-center">
-									<span className="text-nowrap">Distance To The Moon:</span>
-									{vehicleData.motInfo.hasHadMot ? (
-										<DistanceToMoon
-											distance={
-												Number(vehicleData.mileage.allPassedMotMiles[0]?.odometerValue) ?? 0
-											}
-											unit={vehicleData.mileage.motOdometerUnit ?? "mi"}
-											carColour={vehicleData.vehicleInformation.colour ?? "#BABD1A"}
-										/>
-									) : (
-										<DistanceToMoon
-											distance={0}
-											unit={"mi"}
-											carColour={vehicleData.vehicleInformation.colour ?? "#BABD1A"}
-										/>
-									)}
-								</div>
-							</TableCell>
-						</TableRow>
-					</TableBody>
-				</Table>
-			</CardContent>
-		</Card>
+													: "Unknown"}
+											</span>
+										</div>
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										<div className="flex justify-between w-full">
+											<span className="text-nowrap">Average Yearly Mileage:</span>
+											<span className="text-right font-extrabold">
+												{`${averageYearlyMileage} ${vehicleData.mileage.motOdometerUnit}/year`}
+											</span>
+										</div>
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										<div className="flex justify-between w-full">
+											<span className="text-nowrap">Estimated Current Mileage:</span>
+											<span className="text-right font-extrabold">
+												{`${estimatedMileage(weeksSinceLastMot(vehicleData.motInfo.lastMotTestDateShort), averageYearlyMileage, currentMileage)} ${vehicleData.mileage.motOdometerUnit}`}
+											</span>
+										</div>
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>
+										<div className="flex flex-col gap-1.5 justify-between w-full items-center">
+											<span className="text-nowrap">Distance To The Moon:</span>
+											{vehicleData.motInfo.hasHadMot ? (
+												<DistanceToMoon
+													distance={
+														Number(vehicleData.mileage.allPassedMotMiles[0]?.odometerValue) ??
+														0
+													}
+													unit={vehicleData.mileage.motOdometerUnit ?? "mi"}
+													carColour={vehicleData.vehicleInformation.colour ?? "#BABD1A"}
+												/>
+											) : (
+												<DistanceToMoon
+													distance={0}
+													unit={"mi"}
+													carColour={vehicleData.vehicleInformation.colour ?? "#BABD1A"}
+												/>
+											)}
+										</div>
+									</TableCell>
+								</TableRow>
+							</TableBody>
+						</Table>
+					</CardContent>
+				</Card>
+			) : (
+				<div className="flex flex-col items-center gap-1 h-full text-destructive">
+					<p className="text-center md:text-xl text-base font-bold">
+						This vehicle has no recorded mileage.
+					</p>
+					<p className="text-center md:text-xl text-base md:w-3/4 w-full">
+						This site uses recorded MOT data to calculate the mileage of the vehicle.
+						If there is no recorded MOT data, the site will not be able to calculate
+						the mileage of the vehicle.
+					</p>
+				</div>
+			)}
+		</>
 	);
 }
 
