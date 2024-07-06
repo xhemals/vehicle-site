f3789be3b2d1c21b616256dae57cd4ebe872578a
Jamie Speller <j@miee.uk>
2024-07-06 01:52:18 +0100

    mot history index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 			<h1 className="text-center md:text-5xl text-3xl font-bold">
 				Free Car Check
 			</h1>
-			<NumberPlateSearch />
+			<NumberPlateSearch searchPage="vehicle-check" />
 			<div className="flex flex-col items-center md:items-start md:flex-row md:justify-center md:flex-wrap gap-4 h-full">
 				<div className="md:w-[267px] w-[219px]">
 					<h2 className="md:text-left text-center md:text-3xl text-xl font-bold">
@@ -25,7 +25,9 @@ export default function Home() {
 						<li>
 							<Accordion type="single" collapsible>
 								<LiAccordionItem value="item-1">
-									<LiAccordionTrigger>Vehicle Information</LiAccordionTrigger>
+									<LiAccordionTrigger className="text-left">
+										Vehicle Information
+									</LiAccordionTrigger>
 									<LiAccordionContent>
 										<ul className="list-image-[url(/images/circle-check-big-light-mode.svg)] dark:list-image-[url(/images/circle-check-big-dark-mode.svg)] md:text-lg text-sm list-inside">
 											<li>Make</li>
@@ -43,7 +45,9 @@ export default function Home() {
 						<li>
 							<Accordion type="single" collapsible>
 								<LiAccordionItem value="item-1">
-									<LiAccordionTrigger>Vehicle Specs</LiAccordionTrigger>
+									<LiAccordionTrigger className="text-left">
+										Vehicle Specs
+									</LiAccordionTrigger>
 									<LiAccordionContent>
 										<ul className="list-image-[url(/images/circle-check-big-light-mode.svg)] dark:list-image-[url(/images/circle-check-big-dark-mode.svg)] md:text-lg text-sm list-outside ml-6">
 											<li>Engine Size</li>
@@ -61,7 +65,9 @@ export default function Home() {
 						<li>
 							<Accordion type="single" collapsible>
 								<LiAccordionItem value="item-1">
-									<LiAccordionTrigger>MOT Summary</LiAccordionTrigger>
+									<LiAccordionTrigger className="text-left">
+										MOT Summary
+									</LiAccordionTrigger>
 									<LiAccordionContent>
 										<ul className="list-image-[url(/images/circle-check-big-light-mode.svg)] dark:list-image-[url(/images/circle-check-big-dark-mode.svg)] md:text-lg text-sm list-outside ml-6 max-w-inherit">
 											<li>MOTs Passed</li>
@@ -78,7 +84,9 @@ export default function Home() {
 						<li>
 							<Accordion type="single" collapsible>
 								<LiAccordionItem value="item-1">
-									<LiAccordionTrigger>Mileage Summary</LiAccordionTrigger>
+									<LiAccordionTrigger className="text-left">
+										Mileage Summary
+									</LiAccordionTrigger>
 									<LiAccordionContent>
 										<ul className="list-image-[url(/images/circle-check-big-light-mode.svg)] dark:list-image-[url(/images/circle-check-big-dark-mode.svg)] md:text-lg text-sm list-outside ml-6 max-w-inherit">
 											<li>Last known mileage</li>
