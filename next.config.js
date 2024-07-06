84ef4d811c64e2ad021177a3fad091e6f236d4cf
Jamie Speller <j@miee.uk>
2024-07-06 01:48:21 +0100

    Add global redirect

--- next.config.js
+++ next.config.js
@@ -6,18 +6,28 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  transpilePackages: ["geist"],
+	/**
+	 * If you are using `appDir` then you must comment the below `i18n` config out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
+	transpilePackages: ["geist"],
+	async redirects() {
+		return [
+			// Basic redirect
+			{
+				source: "/vehicle-check",
+				destination: "/",
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default config;
